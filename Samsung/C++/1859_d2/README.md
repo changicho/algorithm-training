6765142f68e93d8802be14376ec817bae7c2bf0c
eunuch74 <eunuch74@gmail.com>
2019-09-03 22:20:19 +0900

    풀이 | 삼성_백만장자 프로젝트
    난이도 d2
    끝부터 순회하는 방법으로 풀이
    freopen을 이용해 input.txt파일 이용

---
+++ Samsung/C++/1859_d2/README.md
@@ -0,0 +1,17 @@
+# 1859. 백만 장자 프로젝트
+[링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LrsUaDxcDFAXc&categoryId=AV5LrsUaDxcDFAXc&categoryType=CODE)
+
+### 난이도 d2
+
+## 정리
+
+```cpp
+freopen("input.txt", "r", stdin);
+```
+위 코드를 이용하면 input.txt의 값으로 바로 테스트 가능하다.
+
+앞에서부터 탐색하지 말고, 뒤에서부터 탐색하면 쉽게 풀 수 있다.
+
+맨 마지막날을 end라 하고 현재 기준 target 으로하자.
+n번째 날의 값이 end보다 크다면, n+1번째부터 end까지 전부 팔면 된다.
+target을 n번째로 갱신하고 탐색을 계속한다.
---
+++ Samsung/C++/1859_d2/answer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+
+using namespace std;
+
+long long VALUE[1000000];
+
+long long solution() {
+    long long answer = 0;
+
+    int N, target;
+    vector<int> array;
+
+    cin >> N;
+    array.resize(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> array[i];
+    }
+
+    reverse(array.begin(), array.end());
+
+    target = array[0];
+    for (int i = 1; i < N; i++) {
+        if(array[i]>target){
+            target = array[i];
+            continue;
+        }
+        answer += target - array[i];
+    }
+
+    return answer;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cout<<"#"<<i+1<<" "<<solution()<<endl;
+    }
+
+    return 0;
+}
