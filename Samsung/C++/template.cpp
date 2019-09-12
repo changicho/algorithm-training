0a5d33e44ac188d172f3bbc735beb88b5a21b312
Changi Cho <eunuch74@gmail.com>
2019-09-12 19:59:52 +0900

    추가 : 문제 양식 (소스, README.md)
    코드 작성시 사용하는 작성 패턴 추가.
    문제 폴더에 공부 내용을 정리하는 README.md 파일의 양식 추가

---
+++ Samsung/C++/template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solution(int test_case) { 
+    
+    cout << "#" << test_case << " "; 
+    
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int test_case = 1; teste_case <= T; test_case++) {
+        solution(test_case);
+    }
+
+    return 0;
+}
