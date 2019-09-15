d9f9a15031852c26a0ca294d2c96fb52fe5bea9f
Changi Cho <eunuch74@gmail.com>
2019-09-15 16:40:07 +0900

    작성 : 삼성_5607_조합
    정답률 27%
    정답 코드를 실행시켜도 전부 틀리다고 나와 진행 불가.
    페르마의 소정리를 이용한 방법.
    좀더 보완이 필요하다.

---
+++ Samsung/C++/5607_d3_combination/README.md
@@ -0,0 +1,15 @@
+# 5607. [Professional] 조합
+
+[링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXGKdbqczEDFAUo&categoryId=AWXGKdbqczEDFAUo&categoryType=CODE)
+
+난이도_d3_23%
+
+## 정리
+
+다른 사람이 올려놓은 정답 코드로 실행해도 전부 오답으로 나옴.
+output 에 문제가 있는듯 하다.
+
+[페르마의 소정리 참고](https://kyunstudio.tistory.com/60)
+
+페르마의 소정리를 이용해 구했다고 하는데...
+좀더 공부해야한다.
---
+++ Samsung/C++/5607_d3_combination/answer.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution(int test_case) {
+    int N, R;
+    long long divition = 1234567891;
+    long long index;
+    long long squared;
+    long long denominator;
+
+    cin >> N >> R;
+
+    vector<long long> factorial(N + 1);
+
+    factorial[0] = 1;
+    factorial[1] = 1;
+
+    for (int index = 2; index <= N; index++) {
+        factorial[index] = (factorial[index - 1] * index) % divition;
+    }
+    denominator = (factorial[R] * factorial[N - R]) % divition;
+
+    index = divition - 2;
+    squared = 1;
+
+    // 분모 denominator 의 R-2 제곱 구하기
+    while (index > 0) {
+        if (index % 2 == 1) {
+            squared *= denominator;
+            squared %= divition;
+        }
+        denominator = (denominator * denominator) % divition;
+        index /= 2;
+    }
+
+    long long result =
+        ((factorial[N] % divition) * (squared % divition)) % divition;
+
+    cout << "#" << test_case << " " << result;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int test_case = 1; test_case <= T; test_case++) {
+        solution(test_case);
+    }
+
+    return 0;
+}
