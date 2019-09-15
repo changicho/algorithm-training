4f5fc6cd4a6c49408f2b71ae49e6a04acdcb1790
Changi Cho <eunuch74@gmail.com>
2019-09-15 19:21:59 +0900

    수정 : 추가 cin, cout 빠르게
    cin, cout 를 사용할 때 10 ms 정도 개선한 코드 추가
    test_case 출력 후 \n 문자 출력 추가

---
+++ Samsung/C++/5607_d3_combination/answer_2.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution(int test_case) {
+    int N, R;
+    long long divition = 1234567891;
+    long long answer;
+
+    cin >> N >> R;
+
+    vector<long long> factorial(N + 1);
+
+    factorial[0] = 1;
+    factorial[1] = 1;
+
+    for (int index = 2; index <= N; index++) {
+        factorial[index] = (factorial[index - 1] * index) % divition;
+    }
+
+    // n! / (n-r)! * r!
+    answer = factorial[N] / (factorial[N - R] * factorial[R]);
+    answer %= divition;
+
+    cout << "#" << test_case << " " << answer;
+}
+
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int T;
+    cin >> T;
+
+    for (int test_case = 1; test_case <= T; test_case++) {
+        solution(test_case);
+    }
+
+    return 0;
+}
--- Samsung/C++/template.cpp
+++ Samsung/C++/template.cpp
@@ -1,17 +1,22 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-void solution(int test_case) { 
+void solution(int test_case) {
+
     
-    cout << "#" << test_case << " "; 
-    
+    cout << "#" << test_case << " ";
+    cout << "\n";
 }
 
 int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int T;
     cin >> T;
 
