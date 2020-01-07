9ac4796bc23b8122e0d5966ee78e5cf5e621dcca
changicho <changicho29@gmail.com>
2020-01-07 21:40:22 +0900

    Docs: Change templates of c++
    ?: 사용하는 사이트의 특성에 맞추기 위함.
    answer.cpp의 양식을 변경
    README.md의 양식을 변경

---
+++ templates/c++/boj/answer.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution() { cout << "\n"; }
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
