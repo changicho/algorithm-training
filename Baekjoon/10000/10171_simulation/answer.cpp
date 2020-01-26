5f690cdaee89bc4a268e7abd2813238cb811c461
changicho <changicho29@gmail.com>
2020-01-26 11:35:49 +0900

    풀이: 백준.10171.고양이
    ?: /를 출력하기 위해 // 사용

---
+++ Baekjoon/10000/10171_simulation/answer.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution() {
+  cout << "\\    /\\\n";
+  cout << " )  ( ')\n";
+  cout << "(  /  )\n";
+  cout << " \\(__)|\n";
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
