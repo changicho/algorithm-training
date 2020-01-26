93070381c7da8508fdecefe21d8c3f36f2adcb12
changicho <changicho29@gmail.com>
2020-01-26 11:31:57 +0900

    풀이: 백준.10718.We love kriii
    ?: 구현

---
+++ Baekjoon/10000/10718_simulation/answer.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution() { cout << "강한친구 대한육군\n강한친구 대한육군\n"; }
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
