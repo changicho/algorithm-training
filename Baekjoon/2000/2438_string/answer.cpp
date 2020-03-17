1675a3746a94563241e75bdb33badaf225c3c805
changicho <changicho29@gmail.com>
2020-03-17 21:16:12 +0900

    풀이: 백준.2438.별 찍기 -1
    ?: 반복문을 이용해 풀이

---
+++ Baekjoon/2000/2438_string/answer.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution() {
+  int N;
+
+  cin >> N;
+
+  for (int i = 1; i <= N; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
