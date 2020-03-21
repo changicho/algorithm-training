afc121e41bea02081b5430bc1261bd697a4ade07
changicho <changicho29@gmail.com>
2020-03-22 00:06:38 +0900

    풀이: 백준.11654.아스키 코드
    ?: casting 을 이용

---
+++ Baekjoon/11000/11654_string/answer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+  char c;
+  cin >> c;
+  cout << (int)c << "\n";
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
