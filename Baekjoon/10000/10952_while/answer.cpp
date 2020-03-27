957b56516ad0dd6583342b6fa2d9d06bf4121059
changicho <changicho29@gmail.com>
2020-03-28 07:52:40 +0900

    풀이: 백준.10952.A+B - 5
    ?: while문을 이용해 풀이

---
+++ Baekjoon/10000/10952_while/answer.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution() {
+  int A, B;
+
+  while (cin >> A) {
+    cin >> B;
+
+    if (A + B == 0) {
+      break;
+    }
+    cout << A + B << "\n";
+  }
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
