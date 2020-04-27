8b6bcc555de7ee49798b5d74545dbfc31f58d393
changicho <changicho29@gmail.com>
2020-04-27 17:33:59 +0900

    풀이: 백준.1008.A/B
    ?: 소숫점 자리수 출력을 위해 cin.precision을 이용

---
+++ Baekjoon/1000/1008_simulation/answer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  cout.precision(10);
+
+  double a, b;
+  cin >> a >> b;
+
+  cout << a / b << endl;
+}
