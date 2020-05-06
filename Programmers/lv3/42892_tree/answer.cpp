c8f7953bef9d4e2fecc8848ab3f39de8c4d22807
changicho <changicho29@gmail.com>
2020-05-07 07:03:59 +0900

    풀이: 프로그래머스.42892.길 찾기 게임
    ?: 노드 생성 함수 추가. 리팩토링

--- Programmers/lv3/42892_tree/answer.cpp
+++ Programmers/lv3/42892_tree/answer.cpp
@@ -11,7 +11,7 @@ struct info {
 
 struct node {
   int index;
-  int x;
+  int val;
   struct node *left;
   struct node *right;
 };
@@ -21,7 +21,7 @@ vector<vector<int>> answer(2);
 
 void insert(node *cur, node *newNode) {
   // 왼쪽에  삽입
-  if (cur->x > newNode->x) {
+  if (cur->val > newNode->val) {
     if (cur->left != NULL) {
       insert(cur->left, newNode);
     } else {
@@ -29,7 +29,7 @@ void insert(node *cur, node *newNode) {
     }
   }
   // 오른쪽에 삽입
-  if (cur->x < newNode->x) {
+  if (cur->val < newNode->val) {
     if (cur->right != NULL) {
       insert(cur->right, newNode);
     } else {
@@ -63,6 +63,16 @@ bool compare(info a, info b) {
   return a.x < b.x;
 }
 
+struct node *makeNode(int index, int x) {
+  node *rootNode = new node;
+
+  rootNode->index = index;
+  rootNode->val = x;
+  rootNode->left = rootNode->right = NULL;
+
+  return rootNode;
+}
+
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
   vector<info> nodes;
 
@@ -79,22 +89,12 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
   sort(nodes.begin(), nodes.end(), compare);
 
   info first = nodes.front();
-  node *rootNode = new node;
+  nodes.erase(nodes.begin());
 
-  rootNode->index = first.index;
-  rootNode->x = first.x;
-  rootNode->left = NULL;
-  rootNode->right = NULL;
+  root = makeNode(first.index, first.x);
 
-  root = rootNode;
-
-  for (int i = 1; i < nodes.size(); i++) {
-    info cur = nodes[i];
-
-    node *nextNode = new node;
-    nextNode->index = cur.index;
-    nextNode->x = cur.x;
-    nextNode->left = nextNode->right = NULL;
+  for (info cur : nodes) {
+    node *nextNode = makeNode(cur.index, cur.x);
 
     insert(root, nextNode);
   }
@@ -103,4 +103,31 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
   postorder(root);
 
   return answer;
+}
+
+int main() {
+  vector<vector<int>> nodeinfo = {{5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1},
+                                  {1, 3}, {8, 6},  {7, 2},  {2, 2}};
+  vector<vector<int>> result = {{7, 4, 6, 9, 1, 8, 5, 2, 3},
+                                {9, 6, 5, 8, 1, 4, 3, 2, 7}};
+
+  vector<vector<int>> myresult = solution(nodeinfo);
+
+  cout << "answer : " << endl;
+  for (vector<int> r : result) {
+    for (int a : r) {
+      cout << a << " ";
+    }
+    cout << endl;
+  }
+
+  cout << "myanswer : " << endl;
+  for (vector<int> r : myresult) {
+    for (int a : r) {
+      cout << a << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
 }
