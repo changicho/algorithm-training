d1f7391f73c1d41fd7f4be94b27e8cab809a2c3b
changicho <changicho29@gmail.com>
2020-06-21 17:03:18 +0900

    풀이: 백준.11657.타임머신
    ?: 벨만포드 알고리즘을 이용해 최단거리 갱신
    음의 싸이클이 발생하는 경우를 체크
    최단 거리가 int형을 벗어날 수 있으므로 long long형으로 갱신

--- Baekjoon/11000/11657_graph/answer.cpp
+++ Baekjoon/11000/11657_graph/answer.cpp
@@ -8,53 +8,42 @@
 using namespace std;
 
 struct edge {
-  int end, cost;
+  long long end, cost;
 };
 
 vector<edge> graph[501];
-vector<int> costs;
-int N, M;
-bool isCycle;
-
-void bellman_ford(int start_index) {
-  // initialize
-  for (int i = 1; i <= N; i++) {
-    costs[i] = INF;
-  }
-  costs[start_index] = 0;
-
-  isCycle = false;
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= N; j++) {
-      // current : 현재 간선
-      for (edge current : graph[j]) {
-        int new_val = costs[j] + current.cost;
-        int before_val = costs[current.end];
-
-        if (costs[j] != INF && before_val > new_val) {
-          costs[current.end] = new_val;
-
-          if (i == N) isCycle = true;
-        }
-      }
-    }
-  }
-}
+long long costs[501];
 
 void solution() {
-  cin >> N >> M;
-
+  int N, M;
   int start = 1;
 
-  costs.resize(N + 1);
-  while (M--) {
+  cin >> N >> M;
+  for (int i = 0; i < M; i++) {
     int A, B, C;
     cin >> A >> B >> C;
 
     graph[A].push_back(edge{B, C});
   }
 
-  bellman_ford(start);
+  // initialize
+  for (int i = 1; i <= N; i++) {
+    costs[i] = INF;
+  }
+
+  costs[start] = 0;
+  bool isCycle = false;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= N; j++) {
+      for (edge g : graph[j]) {
+        if (costs[j] != INF && costs[g.end] > g.cost + costs[j]) {
+          costs[g.end] = g.cost + costs[j];
+
+          if (i == N) isCycle = true;
+        }
+      }
+    }
+  }
 
   if (isCycle) {
     cout << "-1\n";
@@ -62,10 +51,7 @@ void solution() {
   }
 
   for (int i = 2; i <= N; i++) {
-    if (costs[i] == INF)
-      cout << "-1\n";
-    else
-      cout << costs[i] << "\n";
+    cout << (costs[i] == INF ? -1 : costs[i]) << "\n";
   }
 }
 
@@ -73,6 +59,8 @@ int main() {
   ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
+
+  // freopen("./data/input.txt", "r", stdin);
 
   solution();
 
