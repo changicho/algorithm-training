0a6ecad2d2e47a76101aa59b7cfb5564286f9a13
changicho <changicho29@gmail.com>
2020-11-24 22:59:56 +0900

    풀이: 백준.2741.N 찍기
    ?: 반복문을 이용해 풀이

---
+++ Baekjoon/2000/2741_list/answer.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void solution() {
+  int N;
+  cin >> N;
+
+  for (int i = 1; i <= N; i++) {
+    cout << i << "\n";
+  }
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  solution();
+
+  return 0;
+}
