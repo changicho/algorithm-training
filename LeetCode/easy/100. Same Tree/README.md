5f561ff6c188cbb1599579b4f5cfb38da703ae7d
changicho <changicho29@gmail.com>
2021-04-04 15:04:25 +0900

    풀이: 릿코드.100.Same Tree
    ?: DFS, BFS를 이용해 풀이

---
+++ LeetCode/easy/100. Same Tree/README.md
@@ -0,0 +1,98 @@
+# 100. Same Tree
+
+[링크](https://leetcode.com/problems/same-tree/)
+
+| 난이도 |
+| :----: |
+|  Easy  |
+
+## 설계
+
+### 시간 복잡도
+
+트리의 노드의 개수는 최대 100개이다.
+
+각 노드를 순회할 때 시간 복잡도는 O(N) 이다.
+
+### 공간 복잡도
+
+BFS로 순회할 경우 queue가 필요하다.
+
+공간 복잡도는 O(N) 이다.
+
+### 너비우선탐색으로 풀이
+
+> 0ms
+
+전위순회를 이용해 트리를 탐색하고, 만약 현재 값이 다른 경우엔 두 트리가 다르다고 판단할 수 있다.
+
+```cpp
+bool isSameTree(TreeNode* p, TreeNode* q) {
+  queue<TreeNode*> q1;
+  queue<TreeNode*> q2;
+
+  q1.push(p);
+  q2.push(q);
+
+  while (!q1.empty() && !q2.empty()) {
+    TreeNode* cur1 = q1.front();
+    TreeNode* cur2 = q2.front();
+
+    q1.pop();
+    q2.pop();
+
+    if (!cur1 && !cur2) {
+      continue;
+    }
+    if (!cur1 || !cur2) {
+      return false;
+    }
+    if (cur1->val != cur2->val) {
+      return false;
+    }
+
+    q1.push(cur1->left);
+    q1.push(cur1->right);
+
+    q2.push(cur2->left);
+    q2.push(cur2->right);
+  }
+
+  return true;
+}
+```
+
+### 깊이 우선 탐색
+
+> 0ms
+
+재귀호출을 이용해 탐색할 수 있다.
+
+```cpp
+class Solution {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) {
+      return true;
+    }
+    if (!q || !p) {
+      return false;
+    }
+    if (p->val != q->val) {
+      return false;
+    }
+
+    bool ret = isSameTree(p->right, q->right) &&
+               isSameTree(p->left, q->left);
+    return ret;
+  }
+};
+```
+
+## 정리
+
+| 내 코드 (ms) |
+| :----------: |
+|      0       |
+
+## 고생한 점
---
+++ LeetCode/easy/100. Same Tree/answer.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// use BFS
+
+class Solution {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    queue<TreeNode*> q1;
+    queue<TreeNode*> q2;
+
+    q1.push(p);
+    q2.push(q);
+
+    while (!q1.empty() && !q2.empty()) {
+      TreeNode* cur1 = q1.front();
+      TreeNode* cur2 = q2.front();
+
+      q1.pop();
+      q2.pop();
+
+      if (!cur1 && !cur2) {
+        continue;
+      }
+      if (!cur1 || !cur2) {
+        return false;
+      }
+      if (cur1->val != cur2->val) {
+        return false;
+      }
+
+      q1.push(cur1->left);
+      q1.push(cur1->right);
+
+      q2.push(cur2->left);
+      q2.push(cur2->right);
+    }
+
+    return true;
+  }
+};
+
+// use DFS
+
+class Solution {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) {
+      return true;
+    }
+    if (!q || !p) {
+      return false;
+    }
+    if (p->val != q->val) {
+      return false;
+    }
+
+    bool ret = isSameTree(p->right, q->right) &&
+               isSameTree(p->left, q->left);
+    return ret;
+  }
+};
