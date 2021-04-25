18037acb7f533c0887eb07a2043b3d8f1e921c37
changicho <changicho29@gmail.com>
2021-04-25 17:43:55 +0900

    chore: Change template of LeetCode

--- templates/c++/leetcode/answer.cpp
+++ templates/c++/leetcode/answer.cpp
@@ -4,15 +4,3 @@
 #include <vector>
 
 using namespace std;
-
-void solution() { cout << "\n"; }
-
-int main() {
-  ios_base ::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
-  solution();
-
-  return 0;
-}
