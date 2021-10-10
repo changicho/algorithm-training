f3df24a6f669efe346de8b07ad27f2281c0f960e
changicho <changicho29@gmail.com>
2021-10-10 13:17:06 +0900

    :art: Add count solved problems shell scripts

---
+++ Samsung/count.sh
@@ -0,0 +1,24 @@
+echo "========================"
+echo "Samsung SW Academy solved count"
+echo
+
+DIR="$( cd "$( dirname "$0" )" && pwd -P )"
+# echo "path : $DIR"
+# echo
+
+count=0
+
+shopt -s dotglob
+shopt -s nullglob
+array=(*/)
+for dir in "${array[@]}"; 
+do 
+  cd $DIR/$dir
+  num=`ls | wc -l`
+  echo "$dir : `expr $num`"
+  count=`expr $count + $num`
+done
+
+echo
+echo "total : $count"
+echo "========================"
---
+++ count.sh
@@ -0,0 +1,19 @@
+echo "count all problems"
+DIR="$( cd "$( dirname "$0" )" && pwd -P )"
+
+# source $DIR/LeetCode/count.sh
+
+cd $DIR/Baekjoon
+sh ./count.sh
+
+cd $DIR/LeetCode
+sh ./count.sh
+
+cd $DIR/GeeksForGeeks
+sh ./count.sh
+
+cd $DIR/LintCode
+sh ./count.sh
+
+cd $DIR/Samsung
+sh ./count.sh
