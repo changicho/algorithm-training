9b7fab59eec5539d14b0d84b00c0394a163fc7b7
changicho <changicho29@gmail.com>
2021-12-07 00:00:48 +0900

    풀이: 릿코드.37.Sudoku Solver
    ?: 백트래킹 풀이 목 인터뷰 내용 추가

--- LeetCode/hard/37. Sudoku Solver/answer.cpp
+++ LeetCode/hard/37. Sudoku Solver/answer.cpp
@@ -5,6 +5,79 @@
 
 using namespace std;
 
+// use DFS with backtracking
+
+class Solution {
+ private:
+  // use backtracking with DFS
+  // we check colFine[index][number], rowFine[index][number],
+  // sectionFine[y][y][number]
+  bool colFine[10][10], rowFine[10][10], sectionFine[3][3][10];
+  // cause we check in a col, there are no duplicated number in same line
+  // same with row, section (3*3)
+  // make answer boolean value
+  bool answer = false;
+
+  // make function DFS(index)
+  void DFS(int index, vector<vector<char>>& board) {
+    // if axis is over than range
+    // that means there are answer
+    // we can make y, x axis using index
+    int y = index / 9, x = index % 9;
+
+    if (index >= 81) {
+      answer = true;
+      return;
+    }
+
+    // if board[y][x] is already filled, increase axis and DFS it
+    if (board[y][x] != '.') {
+      DFS(index + 1, board);
+      return;
+    }
+
+    // in y,x iterate 1 ~ 9 to check it
+    // if can increase axis
+    for (int num = 1; num <= 9; num++) {
+      if (colFine[x][num] == true) continue;
+      if (rowFine[y][num] == true) continue;
+      if (sectionFine[y / 3][x / 3][num] == true) continue;
+
+      colFine[x][num] = true;
+      rowFine[y][num] = true;
+      sectionFine[y / 3][x / 3][num] = true;
+
+      board[y][x] = num + '0';
+      DFS(index + 1, board);
+
+      if (answer) return;
+
+      board[y][x] = '.';
+      colFine[x][num] = false;
+      rowFine[y][num] = false;
+      sectionFine[y / 3][x / 3][num] = false;
+    }
+  }
+
+ public:
+  void solveSudoku(vector<vector<char>>& board) {
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 9; x++) {
+        if (board[y][x] == '.') continue;
+
+        int num = board[y][x] - '0';
+        colFine[x][num] = true;
+        rowFine[y][num] = true;
+        sectionFine[y / 3][x / 3][num] = true;
+      }
+    }
+
+    DFS(0, board);
+  }
+};
+
+// legacy code
+
 struct Axis {
   int y, x;
 };
@@ -57,7 +130,8 @@ class Solution {
     }
 
     for (int number = 1; number <= 9; number++) {
-      if (!col[x][number] && !row[y][number] && !field[getFieldIndex(y, x)][number]) {
+      if (!col[x][number] && !row[y][number] &&
+          !field[getFieldIndex(y, x)][number]) {
         board[y][x] = number + '0';
         col[x][number] = true;
         row[y][number] = true;
@@ -74,7 +148,5 @@ class Solution {
     }
   }
 
-  int getFieldIndex(int y, int x) {
-    return (y / 3) * 3 + x / 3;
-  }
+  int getFieldIndex(int y, int x) { return (y / 3) * 3 + x / 3; }
 };
