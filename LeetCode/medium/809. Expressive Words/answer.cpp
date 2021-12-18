2d8f08d5b62d9398177643b953e5ac645311f945
changicho <changicho29@gmail.com>
2021-12-18 17:34:29 +0900

    풀이: 릿코드.809.Expressive Words
    ?: 투포인터, 4포인터 풀이 추가

--- LeetCode/medium/809. Expressive Words/answer.cpp
+++ LeetCode/medium/809. Expressive Words/answer.cpp
@@ -60,3 +60,72 @@ class Solution {
     return answer;
   }
 };
+
+// use two pointer
+
+class Solution {
+ private:
+  bool check(string target, string word) {
+    int length1 = target.length(), length2 = word.length();
+    int idx2 = 0;
+
+    for (int idx1 = 0; idx1 < length1; idx1++) {
+      if (idx2 < length2 && target[idx1] == word[idx2]) {
+        idx2++;
+      } else if (idx1 > 1 && target[idx1 - 2] == target[idx1 - 1] &&
+                 target[idx1 - 1] == target[idx1]) {
+      } else if (0 < idx1 && idx1 < length1 - 1 &&
+                 target[idx1 - 1] == target[idx1] &&
+                 target[idx1] == target[idx1 + 1]) {
+      } else {
+        return false;
+      }
+    }
+
+    return idx2 == length2;
+  }
+
+ public:
+  int expressiveWords(string s, vector<string> &words) {
+    int answer = 0;
+    for (string &word : words) {
+      if (check(s, word)) answer++;
+    }
+
+    return answer;
+  }
+};
+
+// use four pointer
+
+class Solution {
+ private:
+  bool check(string target, string word) {
+    int length1 = target.length(), length2 = word.length();
+    int i = 0, j = 0;
+
+    while (i < length1 && j < length2) {
+      if (target[i] != word[j]) return false;
+
+      int iNext = i, jNext = j;
+      while (iNext < length1 && target[iNext] == target[i]) iNext++;
+      while (jNext < length2 && word[jNext] == word[j]) jNext++;
+
+      if (iNext - i != jNext - j && iNext - i < max(3, jNext - j)) return false;
+
+      i = iNext, j = jNext;
+    }
+
+    return i == length1 && j == length2;
+  }
+
+ public:
+  int expressiveWords(string s, vector<string> &words) {
+    int answer = 0;
+    for (string &word : words) {
+      if (check(s, word)) answer++;
+    }
+
+    return answer;
+  }
+};
