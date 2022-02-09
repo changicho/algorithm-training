e85dcb2e423033a8687f19d30c5d9c209ddba09e
changicho <changicho29@gmail.com>
2022-02-09 18:06:45 +0900

    풀이: 릿코드.258.Add Digits
    ?: 수학적 풀이 추가

--- LeetCode/easy/258. Add Digits/answer.cpp
+++ LeetCode/easy/258. Add Digits/answer.cpp
@@ -21,3 +21,15 @@ class Solution {
     return num;
   }
 };
+
+// use math
+
+class Solution {
+ public:
+  int addDigits(int num) {
+    if (num == 0) return 0;
+    if (num % 9 == 0) return 9;
+
+    return num % 9;
+  }
+};
