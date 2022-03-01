a3db0f730263fcc728d7b1fccf94d25b68638f2b
changicho <changicho29@gmail.com>
2022-03-02 00:46:15 +0900

    풀이: 릿코드.18.4Sum
    ?: 투 포인터를 이용해 풀이
    이전 풀이방법 중 2개씩 그룹으로 나눠 풀이하는 방법의 경우 TLE가 발생함.
    투 포인터 풀이 코드 리팩토링

--- LeetCode/medium/18. 4Sum/README.md
+++ LeetCode/medium/18. 4Sum/README.md
@@ -10,33 +10,41 @@
 
 ### 시간 복잡도
 
-숫자 배열의 크기는 최대 200이다. 4가지 숫자를 브루트 포스로 구하는 경우 탐색에 들어가는 시간 복잡도는 O(N^4) 이다. 이는 TL이 발생한다.
+숫자 배열의 크기는 최대 200이다. 4가지 숫자를 브루트 포스로 구하는 경우 탐색에 들어가는 시간 복잡도는 O(N^4) 이다. 이는 TLE가 발생한다.
 
-따라서 다음 두가지 방법을 이용해 풀이할 수 있다.
+정렬을 수행하면 투 포인터를 이용해 구할 수 있다. 투 포인터를 2개의 수에 대해 적용하고, 나머지 2개의 수는 브루트 포스를 통해 탐색한다.
 
-- 투 포인터 O(N^3)
-- 두 영역으로 나눔 O(N^3)
+이 경우 2개의 수를 정하는데 O(N^2), 남은 2개의 수를 정하는데 투 포인터로 O(N)의 시간 복잡도로 풀이가 가능하다.
+
+따라서 O(N^3)의 시간 복잡도로 풀이가 가능하다.
+
+두 영역으로 나누는 경우를 생각해보자.
+
+4개의 수를 2개씩 쪼개 hash map에 합과 pair를 저장한다. 이 과정에 O(N^2)의 시간 복잡도를 사용한다. 이후 pair를 탐색하며 hash map에 현재 합과 더해서 target을 만들 수가 있는지 판단한다.
+
+하나의 값마다 pair의 경우의 수는 최대 N^2개까지 생길 수 있다. 따라서 최악의 경우 시간 복잡도는 O(N^4)이므로 제한시간 내에 불가능하다.
 
 ### 공간 복잡도
 
-정렬에 들어가는 시간 복잡도와 정답을 저장할 배열의 시간 복잡도가 소요된다.
+정렬, 정답을 저장할 배열의 공간 복잡도가 필요하다.
 
-- 정렬 : O(N)
-- 해시 : O(N)
+정답을 담을 공간에 O(N)의 공간 복잡도를 사용한다.
+
+정답에서 중복된 경우를 제거하기 위해 hash set을 사용할 수 있다. 이 경우 O(N)의 공간 복잡도를 사용한다.
 
 ### 투 포인터
 
-> set 사용 : 44ms, index로 중복 제거 : 12ms
+| 내 코드 (ms) | 시간 복잡도 | 공간 복잡도 |
+| :----------: | :---------: | :---------: |
+|     151      |   O(N^3)    |    O(N)     |
 
 first, second, third, fourth 네 index를 찾아야한다.
 
 first, second를 브루트 포스로 탐색하며 고정하고 third, fourth를 투 포인터로 탐색해 정답을 찾을 수 있다.
 
-이때 탐색에 들어가는 시간 복잡도는 O(N^3) 이다.
-
 정답이 중복되는 경우를 다음 두가지 방법으로 막을 수 있다.
 
-- set을 이용
+- hash set을 이용
 - 연속되는 값이 같은 경우 건너뜀 (backtracking)
 
 backtracking을 이용하는 경우가 더 빠르다.
@@ -47,39 +55,44 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
   int size = nums.size();
   sort(nums.begin(), nums.end());
 
-  for (int first = 0; first < size - 3; first++) {
+  for (int idx1 = 0; idx1 < size - 3; idx1++) {
     // check duplicate
-    if (first > 0 && nums[first] == nums[first - 1]) continue;
+    if (idx1 > 0 && nums[idx1] == nums[idx1 - 1]) continue;
     // backtracking
-    if (nums[first] + nums[first + 1] + nums[first + 2] + nums[first + 3] > target) break;
-    if (nums[first] + nums[size - 1] + nums[size - 2] + nums[size - 3] < target) continue;
+    if ((long)nums[idx1] + nums[idx1 + 1] + nums[idx1 + 2] + nums[idx1 + 3] >
+        target)
+      break;
+    if ((long)nums[idx1] + nums[size - 1] + nums[size - 2] + nums[size - 3] <
+        target)
+      continue;
 
-    for (int second = first + 1; second < size - 2; second++) {
+    for (int idx2 = idx1 + 1; idx2 < size - 2; idx2++) {
       // check duplicate
-      if (second > first + 1 && nums[second] == nums[second - 1]) continue;
+      if (idx2 > idx1 + 1 && nums[idx2] == nums[idx2 - 1]) continue;
+      long leftPart = nums[idx1] + nums[idx2];
       // backtracking
-      if (nums[first] + nums[second] + nums[second + 1] + nums[second + 2] > target) break;
-      if (nums[first] + nums[second] + nums[size - 1] + nums[size - 2] < target) continue;
+      if (leftPart + nums[idx2 + 1] + nums[idx2 + 2] > target) break;
+      if (leftPart + nums[size - 1] + nums[size - 2] < target) continue;
 
-      int third = second + 1;
-      int fourth = size - 1;
+      int idx3 = idx2 + 1;
+      int idx4 = size - 1;
 
-      while (third < fourth) {
-        int sum = nums[first] + nums[second] + nums[third] + nums[fourth];
+      while (idx3 < idx4) {
+        long sum = leftPart + nums[idx3] + nums[idx4];
 
         if (sum == target) {
-          answers.push_back({nums[first], nums[second], nums[third], nums[fourth]});
+          answers.push_back({nums[idx1], nums[idx2], nums[idx3], nums[idx4]});
 
-          third++;
-          fourth--;
-          // check duplicate
-          while (third < fourth && nums[third] == nums[third - 1]) third++;
-          while (third < fourth && nums[fourth] == nums[fourth + 1]) fourth--;
+          idx3++, idx4--;
+
         } else if (sum < target) {
-          third++;
+          idx3++;
         } else if (sum > target) {
-          fourth--;
+          idx4--;
         }
+        // check duplicate
+        while (idx3 < idx4 && nums[idx3] == nums[idx3 - 1]) idx3++;
+        while (idx3 < idx4 && nums[idx4] == nums[idx4 + 1]) idx4--;
       }
     }
   }
@@ -88,77 +101,25 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
 }
 ```
 
-## 해시
+hash set을 이용해 정답의 경우 중복을 제거할 수 있다.
 
-> 96ms
-
-결국 정답은 first, second, third, fourth 네 index를 찾아야한다.
-
-여기서 first, second로 만들 수 있는 모든 합을 map에 저장한다.
-
-이후 third, fourth를 브루트포스로 검색하며 더했을 때의 값이 이전 map에 저장되어있는지 확인한다.
-
-이 때, index의 중복을 피해야 하므로 map에 저장할 때 index또한 같이 저장한다.
-
-따라서 first, second, third, fourth 값에서 중복되는 index가 없도록 한다.
+이 경우 hash set에 O(N)의 공간 복잡도를 사용하며, 다음과 같이 구현한다.
 
 ```cpp
-class Solution {
- public:
-  vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    set<vector<int>> s;
-    map<int, vector<pair<int, int>>> m;
-
-    for (int first = 0; first < nums.size() - 1; first++) {
-      for (int second = first + 1; second < nums.size(); second++) {
-        m[nums[first] + nums[second]].push_back({first, second});
-      }
+struct VectorHash {
+  size_t operator()(const std::vector<int>& v) const {
+    std::hash<int> hasher;
+    size_t seed = 0;
+    for (int i : v) {
+      seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
     }
-
-    for (int third = 0; third < nums.size() - 1; third++) {
-      for (int fourth = third + 1; fourth < nums.size(); fourth++) {
-        int sum = nums[third] + nums[fourth];
-
-        if (m.find(target - sum) == m.end()) {
-          continue;
-        }
-
-        vector<pair<int, int>> parts = m[target - sum];
-
-        for (pair<int, int> cur : parts) {
-          if (isSame({third, fourth}, cur)) break;
-
-          if (!isSame({third, fourth}, cur)) {
-            vector<int> v = {nums[cur.first], nums[cur.second], nums[third], nums[fourth]};
-            sort(v.begin(), v.end());
-            s.insert(v);
-          }
-        }
-      }
-    }
-
-    vector<vector<int>> answers;
-    for (vector<int> cur : s) {
-      answers.push_back(cur);
-    }
-    return answers;
-  }
-  bool isSame(pair<int, int> p1, pair<int, int> p2) {
-    if (p1.first == p2.first || p1.second == p2.second) {
-      return true;
-    }
-    if (p1.first == p2.second || p2.first == p1.second) {
-      return true;
-    }
-    return false;
+    return seed;
   }
 };
+
+unordered_set<vector<int>, VectorHash> hashSet;
 ```
 
-## 정리
-
-| 내 코드 (ms) |
-| :----------: |
-|      12      |
-
 ## 고생한 점
+
+테스트 케이스가 변경되어 4개의 수의 합이 int형을 넘어서는 경우가 발생했다.
--- LeetCode/medium/18. 4Sum/answer.cpp
+++ LeetCode/medium/18. 4Sum/answer.cpp
@@ -3,54 +3,75 @@
 #include <map>
 #include <set>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-// use two pointer
-
+// use two pointer with hash set
+// time : O(N^3)
+// space : O(N)
 class Solution {
+ private:
+  struct VectorHash {
+    size_t operator()(const std::vector<int>& v) const {
+      std::hash<int> hasher;
+      size_t seed = 0;
+      for (int i : v) {
+        seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+      }
+      return seed;
+    }
+  };
+
  public:
   vector<vector<int>> fourSum(vector<int>& nums, int target) {
     sort(nums.begin(), nums.end());
     int size = nums.size();
 
-    set<vector<int>> s;
+    unordered_set<vector<int>, VectorHash> hashSet;
 
-    for (int first = 0; first < size - 3; first++) {
-      for (int second = first + 1; second < size - 2; second++) {
-        int third = second + 1;
-        int fourth = size - 1;
+    for (int idx1 = 0; idx1 < size - 3; idx1++) {
+      // check duplicate
+      if (idx1 > 0 && nums[idx1] == nums[idx1 - 1]) continue;
 
-        int sum = nums[first] + nums[second];
+      for (int idx2 = idx1 + 1; idx2 < size - 2; idx2++) {
+        // check duplicate
+        if (idx2 > idx1 + 1 && nums[idx2] == nums[idx2 - 1]) continue;
+        int idx3 = idx2 + 1, idx4 = size - 1;
+        long leftPart = nums[idx1] + nums[idx2];
 
-        while (third < fourth) {
-          int curSum = sum + nums[third] + nums[fourth];
+        while (idx3 < idx4) {
+          long curSum = leftPart + nums[idx3] + nums[idx4];
 
           if (curSum == target) {
-            s.insert({nums[first], nums[second], nums[third], nums[fourth]});
+            vector<int> cur = {nums[idx1], nums[idx2], nums[idx3], nums[idx4]};
+            hashSet.insert(cur);
 
-            third++;
-            fourth--;
+            idx3++, idx4--;
+            while (idx3 < idx4 && nums[idx3] == nums[idx3 - 1]) idx3++;
+            while (idx3 < idx4 && nums[idx4] == nums[idx4 + 1]) idx4--;
           } else if (curSum < target) {
-            third++;
+            idx3++;
           } else {
-            fourth--;
+            idx4--;
           }
         }
       }
     }
 
     vector<vector<int>> answers;
-    for (vector<int> cur : s) {
+    for (vector<int> cur : hashSet) {
       answers.push_back(cur);
     }
     return answers;
   }
 };
 
-// use two pointer not use set
-
+// use two pointer
+// time : O(N^3)
+// space : O(1)
 class Solution {
  public:
   vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -58,38 +79,42 @@ class Solution {
     int size = nums.size();
     sort(nums.begin(), nums.end());
 
-    for (int first = 0; first < size - 3; first++) {
+    for (int idx1 = 0; idx1 < size - 3; idx1++) {
       // check duplicate
-      if (first > 0 && nums[first] == nums[first - 1]) continue;
+      if (idx1 > 0 && nums[idx1] == nums[idx1 - 1]) continue;
       // backtracking
-      if (nums[first] + nums[first + 1] + nums[first + 2] + nums[first + 3] > target) break;
-      if (nums[first] + nums[size - 1] + nums[size - 2] + nums[size - 3] < target) continue;
+      if ((long)nums[idx1] + nums[idx1 + 1] + nums[idx1 + 2] + nums[idx1 + 3] >
+          target)
+        break;
+      if ((long)nums[idx1] + nums[size - 1] + nums[size - 2] + nums[size - 3] <
+          target)
+        continue;
 
-      for (int second = first + 1; second < size - 2; second++) {
+      for (int idx2 = idx1 + 1; idx2 < size - 2; idx2++) {
         // check duplicate
-        if (second > first + 1 && nums[second] == nums[second - 1]) continue;
+        if (idx2 > idx1 + 1 && nums[idx2] == nums[idx2 - 1]) continue;
+        long leftPart = nums[idx1] + nums[idx2];
         // backtracking
-        if (nums[first] + nums[second] + nums[second + 1] + nums[second + 2] > target) break;
-        if (nums[first] + nums[second] + nums[size - 1] + nums[size - 2] < target) continue;
+        if (leftPart + nums[idx2 + 1] + nums[idx2 + 2] > target) break;
+        if (leftPart + nums[size - 1] + nums[size - 2] < target) continue;
 
-        int third = second + 1;
-        int fourth = size - 1;
+        int idx3 = idx2 + 1;
+        int idx4 = size - 1;
 
-        while (third < fourth) {
-          int sum = nums[first] + nums[second] + nums[third] + nums[fourth];
+        while (idx3 < idx4) {
+          long sum = leftPart + nums[idx3] + nums[idx4];
 
           if (sum == target) {
-            answers.push_back({nums[first], nums[second], nums[third], nums[fourth]});
+            answers.push_back({nums[idx1], nums[idx2], nums[idx3], nums[idx4]});
 
-            third++;
-            fourth--;
+            idx3++, idx4--;
             // check duplicate
-            while (third < fourth && nums[third] == nums[third - 1]) third++;
-            while (third < fourth && nums[fourth] == nums[fourth + 1]) fourth--;
+            while (idx3 < idx4 && nums[idx3] == nums[idx3 - 1]) idx3++;
+            while (idx3 < idx4 && nums[idx4] == nums[idx4 + 1]) idx4--;
           } else if (sum < target) {
-            third++;
+            idx3++;
           } else if (sum > target) {
-            fourth--;
+            idx4--;
           }
         }
       }
@@ -99,55 +124,61 @@ class Solution {
   }
 };
 
-// use hash
+// use hash (TLE)
 
 class Solution {
+ private:
+  struct VectorHash {
+    size_t operator()(const std::vector<int>& v) const {
+      std::hash<int> hasher;
+      size_t seed = 0;
+      for (int i : v) {
+        seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+      }
+      return seed;
+    }
+  };
+
+  unordered_set<vector<int>, VectorHash> uniqueSums;
+  unordered_map<long, vector<pair<int, int>>> twoSums;
+
+  void makePairs(vector<pair<int, int>>& arr1, vector<pair<int, int>>& arr2,
+                 vector<int>& nums) {
+    for (auto [idx1, idx2] : arr1) {
+      for (auto [idx3, idx4] : arr2) {
+        if (idx1 == idx3 || idx1 == idx4 || idx2 == idx3 || idx2 == idx4)
+          continue;
+
+        vector<int> temp = {nums[idx1], nums[idx2], nums[idx3], nums[idx4]};
+        sort(temp.begin(), temp.end());
+
+        uniqueSums.insert(temp);
+      }
+    }
+  }
+
  public:
   vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    set<vector<int>> s;
-    map<int, vector<pair<int, int>>> m;
+    int size = nums.size();
 
-    for (int first = 0; first < nums.size() - 1; first++) {
-      for (int second = first + 1; second < nums.size(); second++) {
-        m[nums[first] + nums[second]].push_back({first, second});
+    for (int idx1 = 0; idx1 < size; idx1++) {
+      for (int idx2 = idx1 + 1; idx2 < size; idx2++) {
+        twoSums[nums[idx1] + nums[idx2]].push_back({idx1, idx2});
       }
     }
 
-    for (int third = 0; third < nums.size() - 1; third++) {
-      for (int fourth = third + 1; fourth < nums.size(); fourth++) {
-        int sum = nums[third] + nums[fourth];
+    for (auto [key, value] : twoSums) {
+      long other = target - key;
 
-        if (m.find(target - sum) == m.end()) {
-          continue;
-        }
-
-        vector<pair<int, int>> parts = m[target - sum];
-
-        for (pair<int, int> cur : parts) {
-          if (isSame({third, fourth}, cur)) break;
-
-          if (!isSame({third, fourth}, cur)) {
-            vector<int> v = {nums[cur.first], nums[cur.second], nums[third], nums[fourth]};
-            sort(v.begin(), v.end());
-            s.insert(v);
-          }
-        }
+      if (twoSums.find(other) != twoSums.end()) {
+        makePairs(twoSums[key], twoSums[other], nums);
       }
     }
 
-    vector<vector<int>> answers;
-    for (vector<int> cur : s) {
-      answers.push_back(cur);
+    vector<vector<int>> answer;
+    for (vector<int> sum : uniqueSums) {
+      answer.push_back(sum);
     }
-    return answers;
-  }
-  bool isSame(pair<int, int> p1, pair<int, int> p2) {
-    if (p1.first == p2.first || p1.second == p2.second) {
-      return true;
-    }
-    if (p1.first == p2.second || p2.first == p1.second) {
-      return true;
-    }
-    return false;
+    return answer;
   }
 };
