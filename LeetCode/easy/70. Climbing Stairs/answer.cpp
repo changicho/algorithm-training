901b908a36499adebf46376628b7bf7be9adace1
changicho <changicho29@gmail.com>
2022-03-19 00:23:39 +0900

    풀이: 릿코드.70.Climbing Stairs
    ?: 풀이 설명 추가

--- LeetCode/easy/70. Climbing Stairs/answer.cpp
+++ LeetCode/easy/70. Climbing Stairs/answer.cpp
@@ -5,19 +5,44 @@
 
 using namespace std;
 
+// use dynamic programming
+// time : O(N)
+// space : O(N)
 class Solution {
  public:
   int climbStairs(int n) {
-    int dp[50] = {
-        0,
-    };
-    dp[1] = 1;
-    dp[2] = 2;
+    vector<int> dp(45);  // dp[n - 1] : count of n steps
+    dp[0] = 1;
+    dp[1] = 2;
 
-    for (int i = 3; i <= n; i++) {
+    for (int i = 2; i < n; i++) {
       dp[i] = dp[i - 1] + dp[i - 2];
     }
 
-    return dp[n];
+    return dp[n - 1];
+  }
+};
+
+// use dynamic programming (space optimized)
+// time : O(N)
+// space : O(1)
+class Solution {
+ public:
+  int climbStairs(int n) {
+    if (n == 1) {
+      return 1;
+    } else if (n == 2) {
+      return 2;
+    }
+
+    int oneAgo = 2, twoAgo = 1;
+
+    for (int i = 2; i < n; i++) {
+      int cur = oneAgo + twoAgo;
+      twoAgo = oneAgo;
+      oneAgo = cur;
+    }
+
+    return oneAgo;
   }
 };
