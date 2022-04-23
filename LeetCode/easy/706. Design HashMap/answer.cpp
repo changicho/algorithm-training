462d02bcf5f3f6a7cdb393759c7b1e4511bac297
changicho <changicho29@gmail.com>
2022-04-23 16:53:58 +0900

    풀이: 릿코드.706.Design HashMap
    ?: 링크드 리스트를 이용한 풀이 추가

--- LeetCode/easy/706. Design HashMap/answer.cpp
+++ LeetCode/easy/706. Design HashMap/answer.cpp
@@ -11,21 +11,13 @@ using namespace std;
 class MyHashMap {
  public:
   int data[1000001];
-  MyHashMap() {
-    fill(data, data + 1000000, -1);
-  }
+  MyHashMap() { fill(data, data + 1000000, -1); }
 
-  void put(int key, int value) {
-    data[key] = value;
-  }
+  void put(int key, int value) { data[key] = value; }
 
-  int get(int key) {
-    return data[key];
-  }
+  int get(int key) { return data[key]; }
 
-  void remove(int key) {
-    data[key] = -1;
-  }
+  void remove(int key) { data[key] = -1; }
 };
 
 // use linked list hash map
@@ -48,9 +40,7 @@ class MyHashMap {
 
   Node* data[size];
 
-  int hashFunction(int key) {
-    return (int)((long)key * mult % size);
-  }
+  int hashFunction(int key) { return (int)((long)key * mult % size); }
 
  public:
   void put(int key, int val) {
@@ -102,16 +92,60 @@ class MyHashMap {
  public:
   unordered_map<int, int> um;
 
-  void put(int key, int value) {
-    um[key] = value;
-  }
+  void put(int key, int value) { um[key] = value; }
 
   int get(int key) {
     if (um.count(key) > 0) return um[key];
     return -1;
   }
 
+  void remove(int key) { um.erase(key); }
+};
+
+// use linked list hash map
+// time : O(N / K)
+// space : O(K + M)
+class MyHashMap {
+ private:
+  const static int size = 19997;
+  const int mult = 12582917;
+
+  list<pair<int, int>> data[size];
+
+  int hashFunction(int key) { return (int)((long)key * mult % size); }
+
+ public:
+  void put(int key, int val) {
+    if (get(key) != -1) {
+      remove(key);
+    }
+
+    int hashValue = hashFunction(key);
+    data[hashValue].push_back({key, val});
+  }
+
+  int get(int key) {
+    int hashValue = hashFunction(key);
+    auto it = data[hashValue].begin();
+
+    while (it != data[hashValue].end()) {
+      if (it->first == key) return it->second;
+      it++;
+    }
+
+    return -1;
+  }
+
   void remove(int key) {
-    um.erase(key);
+    int hashValue = hashFunction(key);
+    auto it = data[hashValue].begin();
+
+    while (it != data[hashValue].end()) {
+      if (it->first == key) {
+        data[hashValue].erase(it);
+        break;
+      }
+      it++;
+    }
   }
 };
