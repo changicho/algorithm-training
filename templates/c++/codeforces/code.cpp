faf765e0495a5af101d9c8b063a2c32b18539f83
changicho <changicho29@gmail.com>
2022-05-14 20:13:59 +0900

    :art: Create template of codeforces
    codeforces 양식 추가

---
+++ templates/c++/codeforces/code.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  // freopen("./input.txt", "r", stdin);
+
+  return 0;
+}
