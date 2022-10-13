bd2d51f5fb33aef86d811205b06b63e2bb6984d4
Changi Cho <eunuch74@gmail.com>
2022-10-14 00:44:11 +0900

    풀이: 릿코드.2432.The Employee That Worked on the Longest Task
    ?: 순회를 이용한 풀이

--- LeetCode/easy/2432. The Employee That Worked on the Longest Task/answer.cpp
+++ LeetCode/easy/2432. The Employee That Worked on the Longest Task/answer.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 // use sort & one pass
-// time : O(N * log_2(N))
+// time : O(N)
 // space : O(1)
 class Solution {
  public:
@@ -15,8 +15,9 @@ class Solution {
     int answer = logs.front()[0];
     int time = logs.front()[1];
 
-    sort(logs.begin(), logs.end(),
-         [](vector<int> &a, vector<int> &b) { return a[1] < b[1]; });
+    // already sorted
+    // sort(logs.begin(), logs.end(),
+    //      [](vector<int> &a, vector<int> &b) { return a[1] < b[1]; });
 
     for (int i = 1; i < size; i++) {
       int curTime = logs[i][1] - logs[i - 1][1];
