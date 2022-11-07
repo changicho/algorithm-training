7bb5b8ba96961aeb709432bafb3e4456b596f8da
Changi Cho <eunuch74@gmail.com>
2022-11-07 18:06:24 +0900

    풀이: 릿코드.212.Word Search II
    ?: 트라이를 이용한 풀이 업데이트

--- LeetCode/hard/212. Word Search II/answer.cpp
+++ LeetCode/hard/212. Word Search II/answer.cpp
@@ -6,131 +6,9 @@
 
 using namespace std;
 
-// use trie and recursive
-
-class Solution {
- private:
-  class Trie {
-   private:
-    struct TrieNode {
-      struct TrieNode* children[26] = {
-          NULL,
-      };
-
-      bool isEnd = false;
-    };
-
-    TrieNode* root = new TrieNode;
-
-   public:
-    void insert(string word) {
-      TrieNode* node = root;
-
-      for (char c : word) {
-        int index = c - 'a';
-        if (!node->children[index]) {
-          node->children[index] = new TrieNode;
-        }
-
-        node = node->children[index];
-      }
-
-      node->isEnd = true;
-    }
-
-    bool search(string word) {
-      TrieNode* node = root;
-
-      for (char c : word) {
-        int index = c - 'a';
-        if (!node->children[index]) return false;
-
-        node = node->children[index];
-      }
-
-      return node && node->isEnd;
-    }
-
-    bool startsWith(string prefix) {
-      TrieNode* node = root;
-
-      for (char c : prefix) {
-        int index = c - 'a';
-        if (!node->children[index]) return false;
-
-        node = node->children[index];
-      }
-
-      return node;
-    }
-  };
-
-  struct Axis {
-    int y, x;
-  };
-
-  Axis dirs[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-  unordered_set<string> findMap;
-  Trie* trie = new Trie();
-  int rows;
-  int cols;
-
-  void recursive(Axis axis, string line, vector<vector<char>>& board,
-                 vector<vector<bool>>& visited) {
-    if (visited[axis.y][axis.x]) return;
-    visited[axis.y][axis.x] = true;
-
-    line += board[axis.y][axis.x];
-
-    if (line.length() > 10 || !trie->startsWith(line)) {
-      visited[axis.y][axis.x] = false;
-      return;
-    }
-
-    if (trie->search(line)) {
-      findMap.insert(line);
-    }
-
-    for (Axis dir : dirs) {
-      Axis next = {axis.y + dir.y, axis.x + dir.x};
-
-      if (next.y < 0 || next.y >= rows || next.x < 0 || next.x >= cols)
-        continue;
-      if (visited[next.y][next.x]) continue;
-
-      recursive(next, line, board, visited);
-    }
-
-    visited[axis.y][axis.x] = false;
-  }
-
- public:
-  vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-    rows = board.size();
-    cols = board.front().size();
-
-    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-
-    for (string word : words) {
-      trie->insert(word);
-    }
-
-    for (int y = 0; y < rows; y++) {
-      for (int x = 0; x < cols; x++) {
-        recursive({y, x}, "", board, visited);
-      }
-    }
-
-    vector<string> answer;
-    for (string word : findMap) {
-      answer.emplace_back(word);
-    }
-    return answer;
-  }
-};
-
 // use trie and recursive with trieNode
-
+// time : O(R \* C \* 4^L \* L)
+// space : O(N)
 class Solution {
  private:
   class TrieNode {
@@ -144,6 +22,30 @@ class Solution {
   };
 
   class Trie {
+   private:
+    void erase(string& word, int index, TrieNode* cur) {
+      int length = word.length();
+
+      if (index == length) {
+        cur->isEnd = false;
+        return;
+      }
+
+      int key = word[index] - 'a';
+      erase(word, index + 1, cur->children[key]);
+      if (isEmpty(cur->children[key])) {
+        cur->children[key] = NULL;
+        delete cur->children[key];
+      }
+    }
+
+    bool isEmpty(TrieNode* cur) {
+      for (int i = 0; i < 26; ++i) {
+        if (cur->children[i]) return false;
+      }
+      return true;
+    }
+
    public:
     TrieNode* root;
     Trie(vector<string>& words) {
@@ -157,44 +59,17 @@ class Solution {
       TrieNode* cur = root;
 
       for (char& c : word) {
-        int index = c - 'a';
-
-        if (!cur->children[index]) {
-          cur->children[index] = new TrieNode();
+        int key = c - 'a';
+        if (!cur->children[key]) {
+          cur->children[key] = new TrieNode();
         }
-        cur = cur->children[index];
+        cur = cur->children[key];
       }
 
       cur->isEnd = true;
     }
 
     void erase(string word) { erase(word, 0, root); }
-
-   private:
-    void erase(string& word, int index, TrieNode* cur) {
-      int length = word.length();
-
-      if (index == length) {
-        cur->isEnd = false;
-        return;
-      }
-
-      int key = word[index] - 'a';
-
-      erase(word, index + 1, cur->children[key]);
-
-      if (isEmpty(cur->children[key])) {
-        cur->children[key] = NULL;
-        delete cur->children[key];
-      }
-    }
-
-    bool isEmpty(TrieNode* cur) {
-      for (int i = 0; i < 26; ++i) {
-        if (cur->children[i]) return false;
-      }
-      return true;
-    }
   };
 
   struct Axis {
