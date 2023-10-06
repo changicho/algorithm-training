223125695f89b8d5702d90603ed2fa9399ac59d3
changicho <changicho29@gmail.com>
2023-10-07 00:06:17 +0900

    풀이: 릿코드.343.Integer Break
    ?: 동적 계획법 풀이 설명 추가

--- LeetCode/medium/343. Integer Break/answer.cpp
+++ LeetCode/medium/343. Integer Break/answer.cpp
@@ -37,16 +37,13 @@ class Solution {
 class Solution {
  public:
   int integerBreak(int n) {
-    if (n <= 2) return 1;
-
     vector<int> dp(n + 1, 0);
-
     dp[1] = 0;
     dp[2] = 1;
 
     for (int cur = 3; cur <= n; cur++) {
       for (int part = 1; part < cur; part++) {
-        dp[cur] = max(dp[cur], max(part * (cur - part), part * dp[cur - part]));
+        dp[cur] = max({dp[cur], part * (cur - part), part * dp[cur - part]});
       }
     }
     return dp[n];
