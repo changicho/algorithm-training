37dcb1552e8a0591100e04c3bed97d7bcba29b0d
Changi Cho <eunuch74@gmail.com>
2023-12-26 00:04:30 +0900

    풀이: 릿코드.91.Decode Ways
    ?: 동적 계획법을 이용해 풀이

--- LeetCode/medium/91. Decode Ways/README.md
+++ LeetCode/medium/91. Decode Ways/README.md
@@ -29,77 +29,51 @@
 dp 배열을 다음과 같이 설정한다.
 
 ```cpp
-dp[length]; // 길이 length 까지인 문자열에서 가능한 경우의 수
+dp[index]; // index까지 문자열에서 가능한 경우의 수
 ```
-
-(좀더 직관적으로 이해하기 위해 글자를 뒤집은 후 생각한다.)
 
 맨 처음에서 글자를 순회하며 현재 글자의 index에서 가능한 경우의 수는 다음과 같다.
 
 - 현재 index 한 글자를 영어로 치환
-- index, index - 1번째 글자를 영어로 치환
+-  index - 1, index 번째 글자를 영어로 치환
 
 따라서 현재 경우의 수는 다음과 같다.
 
 ```cpp
-dp[length] = dp[length - 1] + dp[length -2];
+// 가능한 경우
+dp[index] = dp[index - 1] + dp[index - 2];
 ```
 
 여기서 두번째 경우는 항상 만족하지 않으므로 조건을 따져야 한다.
 
-먼저 index, index -1 두 글자를 합친 2자리 숫자가 0으로 시작하거나 알파벳 표현 범위를 벗어난 경우에는 두 글자를 영어로 바꿀 수 없다.
-
-초기값은 다음과 같다.
-
-```cpp
-if (s[0] != '0') {
-  dp[1] = 1;
-  dp[2] = 1;
-}
-
-if (s[1] != '0') {
-  int key = (s[1] - '0') * 10 + (s[0] - '0');
-  if (0 < key && key <= 26) dp[2] += 1;
-}
-```
+먼저 index-1, index 두 글자를 합친 2자리 숫자가 0으로 시작하거나 알파벳 표현 범위를 벗어난 경우에는 두 글자를 영어로 바꿀 수 없다.
 
 이후 length 까지 반복하며 정답을 갱신한다.
 
 ```cpp
 int numDecodings(string s) {
+  int size = s.size();
+
   int answer = 0;
-  if (s.front() == '0') return answer;
 
-  int length = s.length();
-  int dp[101] = {1};
-  reverse(s.begin(), s.end());
-
-  if (s.front() != '0') {
-    dp[1] = 1;
-    dp[2] = 1;
+  vector<int> dp(size + 1, 0);
+  if (s[0] != '0') {
+    dp[0] = 1;
   }
 
-  if (length >= 2 && s[1] != '0') {
-    int key = (s[1] - '0') * 10 + (s[0] - '0');
-    if (0 < key && key <= 26) {
-      dp[2] += dp[0];
-    }
-  }
-
-  for (int i = 3; i <= length; i++) {
-    if (s[i - 2] != '0') {
+  for (int i = 1; i < size; i++) {
+    if (s[i] != '0') {
       dp[i] += dp[i - 1];
     }
 
+    int key = (s[i - 1] - '0') * 10 + s[i] - '0';
     if (s[i - 1] == '0') continue;
-    int key = (s[i - 1] - '0') * 10 + (s[i - 2] - '0');
+    if (0 == key || key > 26) continue;
 
-    if (0 < key && key <= 26 && s[i - 3] != '0') {
-      dp[i] += dp[i - 2];
-    }
+    dp[i] += (i - 2 >= 0) ? dp[i - 2] : 1;
   }
 
-  return dp[length];
+  return dp[size - 1];
 }
 ```
 
--- LeetCode/medium/91. Decode Ways/answer.cpp
+++ LeetCode/medium/91. Decode Ways/answer.cpp
@@ -7,78 +7,66 @@
 using namespace std;
 
 // use dynamic programming
-
+// time : O(N)
+// space : O(N)
 class Solution {
  public:
   int numDecodings(string s) {
-    unordered_set<string> S;
+    int size = s.size();
+    unordered_set<string> us;
     for (int i = 1; i <= 26; i++) {
-      S.insert(to_string(i));
+      us.insert(to_string(i));
     }
 
     int answer = 0;
-    if (s.front() == '0') return answer;
-    int length = s.length();
-    int dp[101] = {1};
-    reverse(s.begin(), s.end());
 
-    if (s.front() != '0') {
-      dp[1] = 1;
+    vector<int> dp(size + 1, 0);
+    if (s[0] != '0') {
+      dp[0] = 1;
     }
 
-    for (int i = 2; i <= length; i++) {
-      if (s[i - 2] != '0') {
+    for (int i = 1; i < size; i++) {
+      if (s[i] != '0') {
         dp[i] += dp[i - 1];
       }
 
-      string key = {s[i - 1], s[i - 2]};
-      if (S.find(key) != S.end()) {
-        if (i >= 3 && s[i - 3] == '0') {
-          continue;
-        }
-        dp[i] += dp[i - 2];
-      }
+      string key = {s[i - 1], s[i]};
+      if (us.count(key) == 0) continue;
+
+      dp[i] += (i - 2 >= 0) ? dp[i - 2] : 1;
     }
 
-    return dp[length];
+    return dp[size - 1];
   }
 };
 
 // use dynamic programming without set
-
+// time : O(N)
+// space : O(N)
 class Solution {
  public:
   int numDecodings(string s) {
+    int size = s.size();
+
     int answer = 0;
-    if (s.front() == '0') return answer;
 
-    int length = s.length();
-    int dp[101] = {1};
-    reverse(s.begin(), s.end());
-
-    if (s.front() != '0') {
-      dp[1] = 1;
-      dp[2] = 1;
+    vector<int> dp(size + 1, 0);
+    if (s[0] != '0') {
+      dp[0] = 1;
     }
 
-    if (s[1] != '0') {
-      int key = (s[1] - '0') * 10 + (s[0] - '0');
-      if (0 < key && key <= 26) dp[2] += 1;
-    }
-
-    for (int i = 3; i <= length; i++) {
-      if (s[i - 2] != '0') {
+    for (int i = 1; i < size; i++) {
+      if (s[i] != '0') {
         dp[i] += dp[i - 1];
       }
 
+      int key = (s[i - 1] - '0') * 10 + s[i] - '0';
       if (s[i - 1] == '0') continue;
-      int key = (s[i - 1] - '0') * 10 + (s[i - 2] - '0');
+      if (0 == key || key > 26) continue;
 
-      if (0 < key && key <= 26 && s[i - 3] != '0') {
-        dp[i] += dp[i - 2];
-      }
+      dp[i] += (i - 2 >= 0) ? dp[i - 2] : 1;
     }
 
-    return dp[length];
+    return dp[size - 1];
   }
 };
