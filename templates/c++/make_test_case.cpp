54ec405f0d45427cf33ef9a3d090b71a596f7b35
changicho <changicho29@gmail.com>
2024-02-25 14:55:57 +0900

    🎨 Add test case make code

---
+++ templates/c++/make_test_case.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  freopen("temp_input.txt", "w", stdout);
+
+  int size = 2 * 1e5;
+
+  cout << size << "\n";
+
+  for (int i = 0; i < size; i++) {
+    cout << 2 * 1e5 << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
