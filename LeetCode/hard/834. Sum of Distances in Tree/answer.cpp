18e42b04ddefcc1e6b8becc3b1c1121b5e1ddafa
changicho <changicho29@gmail.com>
2024-04-13 09:23:37 +0900

    풀이: 릿코드.834.Sum of Distances in Tree
    ?: 풀이 내용 추가

--- LeetCode/hard/834. Sum of Distances in Tree/answer.cpp
+++ LeetCode/hard/834. Sum of Distances in Tree/answer.cpp
@@ -49,3 +49,59 @@ class Solution {
     return ans;
   }
 };
+
+// use subtree sum and count
+// time : O(N)
+// space : O(N)
+class Solution {
+ private:
+  vector<int> answer;
+  vector<int> childrenCounts;
+  vector<vector<int>> graph;
+  int size;
+  int distancesFromRoot = 0;
+
+  void preProcess(int node, int parent, int depth) {
+    for (int &child : graph[node]) {
+      if (child == parent) continue;
+
+      preProcess(child, node, depth + 1);
+      childrenCounts[node] += childrenCounts[child];
+    }
+    distancesFromRoot += depth;
+  }
+
+  void recursive(int node, int parent, int curCost) {
+    answer[node] = curCost;
+
+    for (int &child : graph[node]) {
+      if (child == parent) continue;
+
+      int curPartSum = childrenCounts[child];
+      int otherPartSum = size - childrenCounts[child];
+
+      int nextCost = curCost - curPartSum + otherPartSum;
+
+      recursive(child, node, nextCost);
+    }
+  }
+
+ public:
+  vector<int> sumOfDistancesInTree(int n, vector<vector<int>> &edges) {
+    this->size = n;
+    graph.resize(n);
+    answer.resize(n, 0);
+    childrenCounts.resize(n, 1);
+
+    for (vector<int> &edge : edges) {
+      int a = edge.front(), b = edge.back();
+      graph[a].push_back(b);
+      graph[b].push_back(a);
+    }
+
+    preProcess(0, -1, 0);
+    recursive(0, -1, distancesFromRoot);
+
+    return answer;
+  }
+};
