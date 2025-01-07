160ab06802cc9eafcf328f057e1d63fe6cccde56
changicho <changicho29@gmail.com>
2025-01-07 20:57:34 +0900

    풀이: AtCoder/Beginner Contest/AtCoder Beginner Contest 387/A
    ?: 구현을 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 387/A/code.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) { return (a + b) * (a + b); }
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int A, B;
+  cin >> A >> B;
+
+  cout << solution(A, B) << endl;
+
+  return 0;
+}
