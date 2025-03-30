2c8e0fe9431591478333d828ac15c11b7fc35203
changicho <changicho29@gmail.com>
2025-03-30 18:33:09 +0900

    풀이: AtCoder/Beginner Contest/AtCoder Beginner Contest 398/A
    ?: 탐욕 알고리즘을 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 398/A/code.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+  int half = (n + 1) / 2;
+
+  string answer = string(half - 1, '-') + string(n % 2 == 0 ? 2 : 1, '=') +
+                  string(half - 1, '-');
+
+  return answer;
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int N;
+  cin >> N;
+
+  string answer = solution(N);
+
+  cout << answer << endl;
+  // cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
