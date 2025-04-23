6a71995a6b665eaa9dec197e94402245890b95d3
changicho <changicho29@gmail.com>
2025-04-23 21:08:15 +0900

    풀이: AtCoder/Beginner Contest/AtCoder Beginner Contest 402/A
    ?: 순회를 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 402/A/code.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+string solution(string &s) {
+  string answer = "";
+
+  for (char &c : s) {
+    if (isupper(c)) {
+      answer += c;
+    }
+  }
+
+  return answer;
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  string S;
+  cin >> S;
+
+  auto answer = solution(S);
+
+  cout << answer << endl;
+  // cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
