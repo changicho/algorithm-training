d341b29f26a7beb20b96632cfd1b674f14c20038
changicho <changicho29@gmail.com>
2025-05-22 00:09:18 +0900

    풀이: AtCoder/Beginner Contest/AtCoder Beginner Contest 404/A
    ?: 완전 탐색을 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 404/A/code.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+char solution(string s) {
+  for (char c = 'a'; c <= 'z'; c++) {
+    if (s.find(c) == string::npos) {
+      return c;
+    }
+  }
+
+  return 'a';
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  string S;
+  cin >> S;
+
+  char answer = solution(S);
+
+  cout << answer << endl;
+  // cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
