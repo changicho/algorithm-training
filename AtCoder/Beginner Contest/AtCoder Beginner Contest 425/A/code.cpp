9c318727bf48530f81c87b96264658740bf7718e
changicho <changicho29@gmail.com>
2025-10-01 20:51:43 +0900

    풀이: AtCoder Beginner Contest 425/A
    ?: 순회를 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 425/A/code.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+auto solution(int n) {
+  int answer = 0;
+
+  for (int i = 1; i <= n; i++) {
+    answer += i * i * i * (i % 2 == 0 ? +1 : -1);
+  }
+
+  return answer;
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int N;
+  cin >> N;
+
+  auto answer = solution(N);
+
+  cout << answer << endl;
+  // cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
