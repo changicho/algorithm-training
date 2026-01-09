8b326694e53fdedf5dedc56c85c849098e2c1338
changicho <changicho29@gmail.com>
2026-01-09 21:01:36 +0900

    풀이: AtCoder Beginner Contest 437/A
    ?: 계산을 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 437/A/code.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+  int answer = a * 12 + b;
+
+  return answer;
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int A, B;
+  cin >> A >> B;
+
+  auto answer = solution(A, B);
+
+  cout << answer << endl;
+  // cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
