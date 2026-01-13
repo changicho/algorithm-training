f3a79362053f6d91c9dc7d60d4904a4ebe1fe313
changicho <changicho29@gmail.com>
2026-01-13 22:59:28 +0900

    풀이: AtCoder Beginner Contest 440/A
    ?: 연산을 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 440/A/code.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long x, long long y) {
+  long long d = pow(2, y);
+
+  return x * d;
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  long long X, Y;
+  cin >> X >> Y;
+
+  auto answer = solution(X, Y);
+
+  cout << answer << endl;
+  // cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
