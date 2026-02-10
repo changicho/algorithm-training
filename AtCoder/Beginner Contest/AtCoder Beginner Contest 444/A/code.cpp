9682c7890f662cca90ce98bc58a5dfce9cd47fb1
changicho <changicho29@gmail.com>
2026-02-11 00:07:48 +0900

    풀이: AtCoder Beginner Contest 444.A
    ?: 비교를 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 444/A/code.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+bool solution(int n) {
+  string s = to_string(n);
+
+  return s[0] == s[1] && s[1] == s[2];
+}
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int N;
+  cin >> N;
+
+  auto answer = solution(N);
+
+  // cout << answer << endl;
+  cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
