ede8ffa0fa2a62754d127b5c0d689f3b541f76c1
changicho <changicho29@gmail.com>
2026-03-28 23:39:06 +0900

    풀이: 릿코드.3875.Construct Uniform Parity Array I
    ?: 탐욕 알고리즘을 이용해 풀이

---
+++ LeetCode/easy/3875. Construct Uniform Parity Array I/code.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+// greedy
+// time : O(1)
+// space : O(1)
+class Solution {
+ public:
+  bool uniformArray(vector<int>& nums1) { return true; }
+};
