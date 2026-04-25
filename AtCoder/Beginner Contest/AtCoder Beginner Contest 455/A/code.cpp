7b8a467097cc58e5e1066dce8d62e5a0f9aad74d
changicho <changicho29@gmail.com>
2026-04-26 00:25:25 +0900

    풀이: AtCoder Beginner Contest 455/A
    ?: 비교를 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 455/A/code.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+bool solution(int a, int b, int c) { return a != b && b == c; }
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  auto answer = solution(A, B, C);
+
+  // cout << answer << endl;
+  cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
