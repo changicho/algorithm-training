2ededf90c43fd47542a5bc613b7402a2effda9a0
changicho <changicho29@gmail.com>
2026-06-22 00:09:29 +0900

    풀이: AtCoder Beginner Contest 463/A
    ?: 수학을 이용해 풀이

---
+++ AtCoder/Beginner Contest/AtCoder Beginner Contest 463/A/code.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+bool solution(int x, int y) { return x * 9 == y * 16; }
+
+int main() {
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout.precision(10);
+
+  // freopen("./input.txt", "r", stdin);
+
+  int X, Y;
+  cin >> X >> Y;
+
+  auto answer = solution(X, Y);
+
+  // cout << answer << endl;
+  cout << (answer ? "Yes" : "No") << endl;
+  // for (auto &line : answer) {
+  //   cout << line << "\n";
+  // }
+  // cout << endl;
+
+  return 0;
+}
